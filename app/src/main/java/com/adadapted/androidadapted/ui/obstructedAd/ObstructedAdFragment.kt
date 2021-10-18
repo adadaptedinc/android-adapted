f420fb3d69ee5c22977fb33bb284bb9d7365c17b
Brett Clifton <bclifton@adadapted.com>
2021-10-18 00:29:20 -0400

    feat: adding obstruction view ad funcionality

--- app/src/main/java/com/adadapted/androidadapted/ui/obstructedAd/ObstructedAdFragment.kt
+++ app/src/main/java/com/adadapted/androidadapted/ui/obstructedAd/ObstructedAdFragment.kt
@@ -1,19 +1,22 @@
 package com.adadapted.androidadapted.ui.obstructedAd
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.adadapted.android.sdk.ui.view.AaZoneView
 import com.adadapted.androidadapted.databinding.FragmentObstructedAdBinding
 
 class ObstructedAdFragment : Fragment() {
 
     private lateinit var obstructedAdViewModel: ObstructedAdViewModel
     private var _binding: FragmentObstructedAdBinding? = null
+    private var obstructedAdZoneView: AaZoneView? = null
+    private var scrollView: ScrollView? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,11 +32,34 @@ class ObstructedAdFragment : Fragment() {
         _binding = FragmentObstructedAdBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textObstructedAd
-        obstructedAdViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        scrollView = _binding?.scrollView
+
+        scrollView?.setOnScrollChangeListener { view, i, i2, i3, i4 ->
+            val scrollBounds = Rect()
+            scrollView?.getHitRect(scrollBounds)
+            if (obstructedAdZoneView?.getLocalVisibleRect(scrollBounds) == true) {
+                // Any portion of the imageView, even a single pixel, is within the visible window
+                obstructedAdZoneView?.setAdZoneVisibility(true)
+            } else {
+                obstructedAdZoneView?.setAdZoneVisibility(false)
+            }
+        }
+
+        obstructedAdZoneView = _binding?.obstructedAdZoneView
+        obstructedAdZoneView?.init("101930")
+        obstructedAdZoneView?.setAdZoneVisibility(false)
+
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        obstructedAdZoneView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        obstructedAdZoneView?.onStop()
     }
 
     override fun onDestroyView() {
