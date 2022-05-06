9929135a8585001aa3ceca0618bb7fc2680f5e8e
Brett Clifton <bclifton@adadapted.com>
2022-05-06 14:04:39 -0400

    feat: bringing back invisible ads and other temporarily disabled features

--- app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
+++ app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
@@ -3,21 +3,16 @@ package com.adadapted.androidadapted
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
-import android.os.Message
 import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.adadapted.library.AdAdapted
 import com.adadapted.library.AdAdaptedEnv
 import com.adadapted.library.atl.AddToListItem
 
 class ApplicationStartup : Application() {
-
     override fun onCreate() {
         super.onCreate()
         val tag = "AADroidTestApp"
-
 
         //AdAdapted.disableAdTracking(this); //Disable ad tracking completely
         AdAdapted
@@ -27,11 +22,9 @@ class ApplicationStartup : Application() {
             .onHasAdsToServe {
                 Log.i(tag, "Has Ads To Serve: $it")
             }
-//            .setSdkEventListener(object : AaSdkEventListener {
-//                override fun onNextAdEvent(zoneId: String, eventType: String) {
-//                    Log.i(tag, "Ad $eventType for Zone $zoneId")
-//                }
-//            })
+            .setSdkEventListener { zoneId, eventType ->
+                Log.i(tag, "Ad $eventType for Zone $zoneId")
+            }
             .setSdkAddItContentListener {
                 val listItems: List<AddToListItem> = it.getItems()
                 it.itemAcknowledge(listItems.first())
