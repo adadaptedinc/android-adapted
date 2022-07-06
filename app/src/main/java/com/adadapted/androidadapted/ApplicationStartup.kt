a1c305cf5548c28f9db3b5f9f07c59ea344c3816
Brett Clifton <bclifton@adadapted.com>
2022-07-06 11:01:58 -0400

    feat: updates from listener changers

--- app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
+++ app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
@@ -7,7 +7,9 @@ import android.util.Log
 import android.widget.Toast
 import com.adadapted.library.AdAdapted
 import com.adadapted.library.AdAdaptedEnv
-import com.adadapted.library.atl.AddToListItem
+import com.adadapted.library.atl.AddToListContent
+import com.adadapted.library.interfaces.AddItContentListener
+import com.adadapted.library.interfaces.EventBroadcastListener
 
 class ApplicationStartup : Application() {
     override fun onCreate() {
@@ -16,29 +18,33 @@ class ApplicationStartup : Application() {
 
         //AdAdapted.disableAdTracking(this); //Disable ad tracking completely
         AdAdapted
-            .withAppId("NWY0NTM2YZDMMDQ0") // #YOUR API KEY GOES HERE# NTDMZJK2NTM2YWZH //OG Prod //NWY0NTM2YZDMMDQ0 test
+            .withAppId("NWY0NTM2YZDMMDQ0") // #YOUR API KEY GOES HERE# //NWY0NTM2YZDMMDQ0 test
             .inEnvironment(AdAdaptedEnv.DEV)
             .enableKeywordIntercept(true)
             .enableDebugLogging()
             .onHasAdsToServe {
                 Log.i(tag, "Has Ads To Serve: $it")
             }
-            .setSdkEventListener { zoneId, eventType ->
-                Log.i(tag, "Ad $eventType for Zone $zoneId")
-            }
-            .setSdkAddItContentListener {
-                val listItems = it.getItems()
-                it.itemAcknowledge(listItems.first())
-                it.acknowledge()
-
-                Handler(Looper.getMainLooper()).post {
-                    for (item in listItems) {
-                        AddToListItemCache.holdingItems.add(item)
-                    }
-                    AddToListItemCache.takeHoldingItems()
-                    Toast.makeText(this.applicationContext, "Received item: " + listItems.first().title, Toast.LENGTH_SHORT).show()
+            .setSdkEventListener(object: EventBroadcastListener {
+                override fun onAdEventTracked(zoneId: String, eventType: String) {
+                    Log.i(tag, "Ad $eventType for Zone $zoneId")
                 }
-            }
+            })
+            .setSdkAddItContentListener(object: AddItContentListener{
+                override fun onContentAvailable(content: AddToListContent) {
+                    val listItems = content.getItems()
+                    content.itemAcknowledge(listItems.first())
+                    content.acknowledge()
+
+                    Handler(Looper.getMainLooper()).post {
+                        for (item in listItems) {
+                            AddToListItemCache.holdingItems.add(item)
+                        }
+                        AddToListItemCache.takeHoldingItems()
+                        Toast.makeText(applicationContext, "Received item: " + listItems.first().title, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
             .start(this)
     }
 }
