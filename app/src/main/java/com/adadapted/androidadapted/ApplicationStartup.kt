a0d82764df71936f4660bb408f9ee9ab5b11862c
Brett Clifton <bclifton@adadapted.com>
2022-11-22 15:18:23 -0500

    feat: some new settings for testing

--- app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
+++ app/src/main/java/com/adadapted/androidadapted/ApplicationStartup.kt
@@ -19,7 +19,7 @@ class ApplicationStartup : Application() {
 
         //AdAdapted.disableAdTracking(this); //Disable ad tracking completely
         AdAdapted
-            .withAppId("846ACA0X62F13A62") // #YOUR API KEY GOES HERE#  //NWY0NTM2YZDMMDQ0 test //new plat  NWZHZWYWN2U2ODBH //new plat KI NTKXMZFJZTA2NMZJ 846ACA0X62F13A62
+            .withAppId("7D58810X6333241C") // #YOUR API KEY GOES HERE#  //NWY0NTM2YZDMMDQ0 test //new plat  NWZHZWYWN2U2ODBH //new plat KI NTKXMZFJZTA2NMZJ 846ACA0X62F13A62
             .inEnvironment(AdAdaptedEnv.DEV)
             .enableKeywordIntercept(true)
             .enableDebugLogging()
